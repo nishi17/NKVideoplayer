b374ddea9f7b8c69e4bf1e6884144a44abfc7c8c
Nishi <nishideveloper17@gmail.com>
2019-04-10 17:36:29 +0530

    changes

--- app/src/main/java/com/nishi/developer/nkvideoplayer/MainActivity.java
+++ app/src/main/java/com/nishi/developer/nkvideoplayer/MainActivity.java
@@ -2,29 +2,46 @@ package com.nishi.developer.nkvideoplayer;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -143,6 +160,7 @@ public class MainActivity extends AppCompatActivity {
 
 
       }*/
+
     private Cursor videocursor;
 
     private int video_column_index;
@@ -159,9 +177,7 @@ public class MainActivity extends AppCompatActivity {
 
     private final int MY_PERMISSIONS_REQUEST_GALLERY = 0;
 
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -228,11 +244,36 @@ public class MainActivity extends AppCompatActivity {
                         ActivityCompat.requestPermissions(this, permission_gallery, MY_PERMISSIONS_REQUEST_GALLERY);
 
                     } else {
-                        // goToSettings("read gallery", 200);
+                        goToSettings("read gallery", 200);
                     }
                 }
                 break;
         }
+    }
+
+
+    private void goToSettings(String title, final int per) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle("App Permission");
+        builder.setMessage("This app needs " + title + " permission.");
+        builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivityForResult(intent, per);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
     }
 
     @Override
@@ -240,17 +281,18 @@ public class MainActivity extends AppCompatActivity {
 
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK) {
+        if (resultCode == 200) {
 
             init_phone_video_grid();
 
         } else {
 
+            goToSettings("read gallery", 200);
+
         }
 
     }
 
-    @SuppressWarnings("deprecation")
     private void init_phone_video_grid() {
 
         System.gc();
@@ -258,6 +300,12 @@ public class MainActivity extends AppCompatActivity {
         String[] proj = {MediaStore.Video.Media._ID, MediaStore.Video.Media.DATA, MediaStore.Video.Media.DISPLAY_NAME, MediaStore.Video.Media.SIZE};
 
         videocursor = managedQuery(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, proj, null, null, null);
+
+        String selection = MediaStore.Video.Media.DATA + " like?";
+        String[] selectionArgs = new String[]{"%FolderName%"};
+        String[] parameters = {MediaStore.Video.Media._ID};
+        videocursor = managedQuery(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                proj, null, null, MediaStore.Video.Media.DATE_TAKEN + " DESC");
 
         count = videocursor.getCount();
 
@@ -267,11 +315,13 @@ public class MainActivity extends AppCompatActivity {
 
         videolist.setOnItemClickListener(videogridlistener);
 
+
     }
 
+
     private AdapterView.OnItemClickListener videogridlistener = new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView parent, View v, int position,
-                                long id) {
+
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
 
             System.gc();
 
@@ -309,7 +359,7 @@ public class MainActivity extends AppCompatActivity {
             return position;
         }
 
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
 
             System.gc();
 
@@ -324,6 +374,34 @@ public class MainActivity extends AppCompatActivity {
                 convertView = LayoutInflater.from(vContext).inflate(R.layout.list_item, parent, false);
 
                 holder = new ViewHolder();
+/*
+                holder.ll_video_view = (LinearLayout) convertView.findViewById(R.id.ll_video_view);
+
+                holder.ll_video_view.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+
+
+                        video_column_index = videocursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+
+                        videocursor.moveToPosition(position);
+
+                        String filPAth = videocursor.getString(video_column_index);
+
+
+                        Uri filePathUri;
+
+                        int column_index = videocursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);//Instead of "MediaStore.Images.Media.DATA" can be used "_data"
+                        filePathUri = Uri.parse(videocursor.getString(column_index));
+                        String fileName1 = filePathUri.getLastPathSegment().toString();
+
+
+                        RenameFile(fileName1, filPAth);
+                        Log.e("before name " + filPAth, "after name " + fileName1);
+
+                        return true;
+                    }
+                });*/
 
                 holder.txtTitle = (TextView) convertView.findViewById(R.id.txtTitle);
 
@@ -337,21 +415,32 @@ public class MainActivity extends AppCompatActivity {
 
                 id = videocursor.getString(video_column_index);
 
+                holder.txtTitle.setText(id);
+
                 video_column_index = videocursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE);
 
                 videocursor.moveToPosition(position);
 
-                // id += " Size(KB):" +
-                // videocursor.getString(video_column_index);
+                long fileSizeInBytes = videocursor.getLong(video_column_index);
+
+                // Convert the bytes to Kilobytes (1 KB = 1024 Bytes)
+                long fileSizeInKB = fileSizeInBytes / 1024;
+
+                // Convert the KB to MegaBytes (1 MB = 1024 KBytes)
+                long fileSizeInMB = fileSizeInKB / 1024;
 
 
-                int i = video_column_index / 1024;
+                DecimalFormat df2 = new DecimalFormat(".##");
 
+                if (fileSizeInKB > 1024) {
 
-                holder.txtTitle.setText(id);
+                    //double v = df2.format(fileSizeInMB);
+                    holder.txtSize.setText(" Size : " + df2.format(fileSizeInMB) + " MB "/*videocursor.getString(video_column_index)*/);
 
-                holder.txtSize.setText(" Size(KB):" + videocursor.getString(video_column_index));
+                } else {
 
+                    holder.txtSize.setText(" Size : " + fileSizeInKB + " KB "/*videocursor.getString(video_column_index)*/);
+                }
                 String[] proj = {MediaStore.Video.Media._ID, MediaStore.Video.Media.DISPLAY_NAME, MediaStore.Video.Media.DATA};
 
                 try {
@@ -385,7 +474,102 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    private void RenameFile(String oldfileName, final String path) {
+
+        final Dialog saveDialog = new Dialog(MainActivity.this);
+        saveDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        saveDialog.setContentView(R.layout.dialog_file_save);
+        saveDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        saveDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        saveDialog.getWindow().setGravity(Gravity.CENTER);
+        saveDialog.setCanceledOnTouchOutside(false);
+        saveDialog.setCancelable(false);
+
+        final EditText edt_file_name = saveDialog.findViewById(R.id.edt_file_name);
+        edt_file_name.setText(oldfileName);
+        edt_file_name.setSelection(edt_file_name.getText().toString().trim().length());
+
+        Button btn_delete = saveDialog.findViewById(R.id.btn_delete);
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                File f = new File(path);
+
+                if (f.exists()) {
+                    f.delete();
+
+                    Toast.makeText(MainActivity.this, "Delete file", Toast.LENGTH_SHORT).show();
+                }
+
+                saveDialog.dismiss();
+                // finish();
+
+            }
+        });
+
+        Button btn_ok = saveDialog.findViewById(R.id.btn_ok);
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                hideKeyboard(MainActivity.this);
+
+                if (rename(edt_file_name.getText().toString().trim(), path)) {
+
+                    try {
+                        //mDatabase.addRecording(edt_file_name.getText().toString().trim(), fileName, mElapsedMillis);
+
+                        Toast.makeText(MainActivity.this, "File save.", Toast.LENGTH_SHORT).show();
+
+                    } catch (Exception e) {
+                        Log.e("exception", "exception", e);
+                    }
+
+                    //  mElapsedMillis = 0;
+
+                    saveDialog.dismiss();
+
+                    // finish();
+                }
+
+
+            }
+        });
+
+        saveDialog.show();
+
+
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public boolean rename(String name, String strOldPath) {
+
+        File sdcard = Environment.getExternalStorageDirectory();
+
+        File f = new File(strOldPath);
+        File from = new File(f, "from.txt");
+        File to = new File(sdcard, "to.txt");
+        from.renameTo(to);
+
+        return true;
+
+    }
+
     static class ViewHolder {
+
+        LinearLayout ll_video_view;
 
         TextView txtTitle;
 
